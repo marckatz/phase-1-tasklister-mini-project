f7d04b040ae0548f8cd2b223d1bee24864325a1c
Marc Katz <marc.f.katz@gmail.com>
2023-05-15 02:21:55 -0400

    main section of lab, priority dropdown

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+   document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newTask = document.createElement("li");
+    newTask.textContent = e.target.new_task_description.value;
+    newTask.className = e.target.priority.value;
+
+    let deleteButton = document.createElement("input");
+    deleteButton.type = "button";
+    deleteButton.className = "delete"
+    deleteButton.value = "X";
+    newTask.appendChild(deleteButton);
+    deleteButton.addEventListener("click", (e) =>{
+        newTask.remove();
+    });
+
+    document.getElementById("tasks").appendChild(newTask);
+   });     
 });
